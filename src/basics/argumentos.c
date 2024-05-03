7d33788153f7b222ee4796f71ee9e1e888e55a4c
tgvp <thiago.gustavo@gmail.com>
2024-05-03 05:03:03 +0100

    arguments in C

---
+++ src/basics/argumentos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Número de argumentos: %d\n", argc);
+
+    // Imprime cada argumento
+    for (int i = 0; i < argc; i++) {
+        printf("Argumento %d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
