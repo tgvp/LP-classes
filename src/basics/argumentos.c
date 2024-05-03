6f761964307d47c36911a4f8925d927cbf8cc5ea
tgvp <thiago.gustavo@gmail.com>
2024-05-03 05:05:42 +0100

    arguments in C

--- src/basics/argumentos.c
+++ src/basics/argumentos.c
@@ -3,6 +3,11 @@
 int main(int argc, char *argv[]) {
     printf("Número de argumentos: %d\n", argc);
 
+    if (argc < 2) {
+        printf("Nenhum argumento passado\n");
+        return 1;
+    }
+
     // Imprime cada argumento
     for (int i = 0; i < argc; i++) {
         printf("Argumento %d: %s\n", i, argv[i]);
